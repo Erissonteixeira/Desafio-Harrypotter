ba40af7817a3fa4e13e550fec01cc43a85824a69
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-11-11 09:43:16 -0300

    first commit

---
+++ Projeto/src/model/Bruxo.java
@@ -0,0 +1,30 @@
+package model;
+
+public class Bruxo{
+    private String nome;
+    private String casa;
+
+    public Bruxo(String nome, String casa){
+        this.nome = nome;
+        this.casa = casa;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getCasa(){
+        return casa;
+    }
+
+    public void setCasa(String casa){
+        this.casa = casa;
+    }
+    public void mostrarInformacoes(){
+        System.out.println("Bruxo: " + nome + " | casa: " + casa);
+    }
+}
