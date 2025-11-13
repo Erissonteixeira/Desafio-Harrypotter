6251910d08ddb17c27601a760a77bc6eb59180b4
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-11-13 13:04:42 -0300

    feat(dto): adicionar BruxoResponseDto para representar dados de resposta do bruxo

---
+++ Projeto/src/dto/BruxoResponseDto.java
@@ -0,0 +1,40 @@
+package dto;
+
+public class BruxoResponseDto{
+    private String nome;
+    private String casa;
+    private String tipoMagia;
+
+    public BruxoResponseDto(){
+    }
+
+    public BruxoResponseDto(String nome, String casa, String tipoMagia){
+        this.nome = nome;
+        this.casa = casa;
+        this.tipoMagia = tipoMagia;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getCasa(){
+        return casa;
+    }
+
+    public void setCasa(String casa){
+        this.casa = casa;
+    }
+
+    public String getTipoMagia(){
+        return tipoMagia;
+    }
+
+    public void setTipoMagia(String tipoMagia){
+        this.tipoMagia = tipoMagia;
+    }
+}
