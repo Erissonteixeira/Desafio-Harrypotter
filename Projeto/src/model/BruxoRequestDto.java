5826eb9f1d605d3c59f8a6d7b6dadfba85cb2b9b
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-11-13 12:38:26 -0300

    feat(dto): adicionar BruxoRequestDto para representar dados de requisição

---
+++ Projeto/src/model/BruxoRequestDto.java
@@ -0,0 +1,30 @@
+package model;
+
+public class BruxoRequestDto{
+    private String nome;
+    private String casa;
+
+    public BruxoRequestDto(){
+    }
+
+    public BruxoRequestDto(String nome, String casa){
+        this.nome = nome;
+        this.casa = casa;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getCasa(){
+        return casa;
+    }
+
+    public void setCasa(String casa){
+        this.casa = casa;
+    }
+}
